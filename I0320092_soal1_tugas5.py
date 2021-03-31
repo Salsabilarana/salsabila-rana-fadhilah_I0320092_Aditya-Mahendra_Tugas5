d44e65065b04a110d6a22863501ec4b083ef6b01
Salsabila Rana <slsblarana12@gmail.com>
2021-04-01 02:23:28 +0800

    Tugas 5

---
+++ I0320092_soal1_tugas5.py
@@ -0,0 +1,9 @@
+nama = str(input('Harap masukan nama anda: '))
+jenis = str(input('Harap masukan jenis kelamin anda P/L: '))
+
+# memeriksa bilangan
+if jenis =="P":
+    print('Selamat datang, Nyonya', nama )
+else:
+    print('Selamat datang, Tuan', nama )
+print()
